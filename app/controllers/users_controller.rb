a9b151b79a9a283e6f78bf0e2d908ffbfd166a25
Sam Eckmeier <eckmeier41@gmail.com>
2015-06-03 22:53:42 -0400

    minor chabges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
   	  if @user.save
         sign_in @user
-        flash[:success] = "Welcome to the Sample App!"
+        flash[:success] = ""
   		  redirect_to @user
   	  else
   		  render 'new'
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     unless current_user?(user)
       user.destroy
-      flash[:success] = "User deleted."
+      flash[:success] = "Your account has been deleted"
     end
     redirect_to users_url
   end
