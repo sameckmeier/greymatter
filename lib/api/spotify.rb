de0f10996146a0bad100185c9dd0e3734b7cc09f
Sam Eckmeier <eckmeier41@gmail.com>
2015-10-27 22:47:34 -0400

    adds methods to get artist, albums, and to search

--- lib/api/spotify.rb
+++ lib/api/spotify.rb
@@ -1,36 +1,69 @@
 module Api
   module Spotify
 
-    BASE_URL = "https://api.spotify.com/v1/search"
+    BASE_URL = "https://api.spotify.com/v1"
 
-    def search(args)
+    def search(query)
       res = []
 
-      query = "?type=#{args[:type].join('%20')}&q=#{args[:query].gsub!(\s, '%20')}"
-      response = HttParty.get("#{BASE_URL}#{query}")
-      json = format_json(response.body)
+      query = "search?type=artist,album&q=#{query.gsub!(\s, '%20')}"
+      response = HttParty.get("#{BASE_URL}/#{query}")
+
+      albums = JSON.parse(response.body, symbolize_names: true)[:albums][:items]
+      artists = JSON.parse(response.body, symbolize_names: true)[:artists][:items]
+      res = albums.concat(artists).map { |json| translate_search_json(json) }
 
       res
     end
 
-    def get(type, id)
-    end
+    def artist(id)
+      res = nil
 
-    def format_json(raw_json)
-      res = []
-
-      res = JSON.parse(raw_json)
-      res = res[:items]
-      res = res.map { |item| unpack_item(item) }
+      response = HttParty.get("#{BASE_URL}/artists/#{id}")
+      json = JSON.parse(response.body, symbolize_names: true)
+      res = translate_artist_json(json)
 
       res
     end
 
-    def unpack_item(item)
-      { name: item[:name], type: item[:type], id: item[:id], source: :spotify }
+    def album(id)
+      res = nil
+
+      response = HttParty.get("#{BASE_URL}/albums/#{id}")
+      json = JSON.parse(response.body, symbolize_names: true)
+      res = translate_album_json(json)
+
+      res
     end
 
-    def to_active_record(spotify_json)
+    def artists_albums(id)
+      res = nil
+
+      response = HttParty.get("#{BASE_URL}/artists/#{id}/albums")
+      json = JSON.parse(response.body, symbolize_names: true)
+      res = translate_album_json(json)
+
+      res
+    end
+
+    def translate_search_json(json)
+      res = { name: json[:name], id: json[:id], source: :spotify }
+
+      if json[:type] == :artist
+        res[:model] = ::Artist
+      elsif json[:type] == :album
+        res[:model] = ::Album
+      end
+
+      res
+    end
+
+    def translate_artist_json(json)
+      { name: json[:name], id: json[:id], model: ::Artist, source: :spotify }
+    end
+
+    def translate_album_json(json)
+      { name: json[:name], id: json[:id], model: ::Album, source: :spotify }
     end
 
   end
