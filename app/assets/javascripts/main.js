7abfb96d6a632bb9775781d767a9a657cf276384
Fabion Stephens <f.o.stephens@gmail.com>
2015-11-30 17:11:01 -0500

    Working on login/register modal

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,23 @@
 $(function(){
     $(document).foundation();
+
+    //tabs
+    $('.tabs li').on('click', function(){
+        var content_to_show = $(this).data('content');
+
+        //get ID from wrapping block
+        var current_context = $(this).closest('.block').attr('id');
+
+        //remove previous active classes
+        $('#'+current_context+' .tabs li').removeClass('active');
+
+        //add active class to current tab
+        $(this).addClass('active');
+
+        //hide content
+        $('#'+current_context+' .tab__content').removeClass('active');
+
+        //show content
+        $('#'+content_to_show).addClass('active');
+    });
 });
